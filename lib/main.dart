c6e87d0ac373e33054dfa8de229accb3ebce8ae0
crpoudyal <crpoudyal@gmail.com>
2023-03-26 17:06:45 +0545

    feat: splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screen/help_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HelpScreen(),
     );
   }
 }
--- lib/screen/help_screen.dart
+++ lib/screen/help_screen.dart
@@ -1,15 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+import 'package:weather_app/screen/home_screen.dart';
+
+class HelpScreen extends StatefulWidget {
+  const HelpScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<HelpScreen> createState() => _HelpScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _HelpScreenState extends State<HelpScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // After 5 sec moves to HomeScreen
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/bg_frame.png'),
+          fit: BoxFit.fill,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.cloud_outlined,
+              size: 100,
+            ),
+            const Text(
+              "we show weather for you",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                },
+                child: const Text("Skip"))
+          ],
+        ),
+      ),
+    );
   }
 }
